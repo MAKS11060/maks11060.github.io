ee41c44aceb67da9ef21d503200c41f3178971c2
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2023-07-26 15:04:00 +0200

    add 404 redirect

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
-import Background from './component/Background.svelte'
+	import Background from './component/Background.svelte'
+
+	const uri = new URL(location.href)
+	if (uri.searchParams.has('status')) {
+		const status = Number(uri.searchParams.get('status'))
+		if (status === 404) {
+			uri.searchParams.delete('status')
+			console.log('from', status)
+			history.replaceState(null, '', uri.searchParams.get('path'))
+		}
+	}
 </script>
 
+<Background/>
+<header class="border-b">
+	<nav>
+		<ul>
+			{#each [
+				{href: '/', name: 'Main'}
+			] as {href, name}}
+				<li class="flex"><a class="p-2" {href}>{name}</a></li>
+			{/each}
+		</ul>
+	</nav>
+</header>
 <main class="flex justify-center">
-	<Background/>
 </main>
