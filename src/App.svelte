146a891b5dda225735475c38a761ace53f6e9284
MAKS11060 <31521952+MAKS11060@users.noreply.github.com>
2023-12-26 18:58:13 +0200

    fmt

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
-	import Background from './component/Background.svelte'
-	import Layout from './layout/Layout.svelte'
-	import {pages} from './store.ts'
+  import Background from './component/Background.svelte'
+  import Layout from './layout/Layout.svelte'
+  import {pages} from './store.ts'
 
-	const uri = new URL(location.href)
-	if (uri.searchParams.has('status')) {
-		const status = Number(uri.searchParams.get('status'))
-		if (status === 404) {
-			uri.searchParams.delete('status')
-			console.log('from', status)
-			history.replaceState(null, '', uri.searchParams.get('path'))
-		}
-	}
+  const uri = new URL(location.href)
+  if (uri.searchParams.has('status')) {
+    const status = Number(uri.searchParams.get('status'))
+    if (status === 404) {
+      uri.searchParams.delete('status')
+      console.log('from', status)
+      history.replaceState(null, '', uri.searchParams.get('path'))
+    }
+  }
 </script>
 
 <Layout>
-	{#each [...pages] as {component, href}}
-		{#if location.pathname.startsWith(href)}
-			<svelte:component this={component}/>
-		{/if}
-	{/each}
+  {#each [...pages] as { component, href }}
+    {#if location.pathname.startsWith(href)}
+      <svelte:component this={component} />
+    {/if}
+  {/each}
 </Layout>
 
-{#if !pages.find(v => location.pathname.startsWith(v.href))}
-	<Background/>
+{#if !pages.find((v) => location.pathname.startsWith(v.href))}
+  <Background />
 {/if}
--- src/component/canvas/Canvas2.svelte
+++ src/component/canvas/Canvas2.svelte
@@ -1,78 +1,77 @@
 <script lang="ts">
-	import {onMount} from 'svelte'
+  import {onMount} from 'svelte'
   import {fRender} from '../../lib/utils.ts'
 
-	let canvas: HTMLCanvasElement
-	let pTime = 0
-	let fps
+  let canvas: HTMLCanvasElement
+  let pTime = 0
+  let fps
 
-	onMount(() => {
-		const ctx = canvas.getContext('2d')
-		let t = 0
+  onMount(() => {
+    const ctx = canvas.getContext('2d')
+    let t = 0
 
-		return fRender(time => {
-			const {width, height} = canvas
+    return fRender((time) => {
+      const {width, height} = canvas
 
-			ctx.clearRect(0,0, width, height)
+      ctx.clearRect(0, 0, width, height)
 
-			t += .3
+      t += 0.3
 
-			// grid
-			// ctx.strokeStyle = 'gray'
-			// for (let i = 0; i < width; i += 50) {
-			// 	for (let j = 0; j < height; j += 50) {
-			// 		ctx.strokeRect(i, j, 50, 50)
-			// 	}
-			// }
+      // grid
+      // ctx.strokeStyle = 'gray'
+      // for (let i = 0; i < width; i += 50) {
+      // 	for (let j = 0; j < height; j += 50) {
+      // 		ctx.strokeRect(i, j, 50, 50)
+      // 	}
+      // }
 
-			// y center line
-			ctx.strokeStyle = 'red'
-			ctx.beginPath()
-			ctx.moveTo(width / 2, 0)
-			ctx.lineTo(width / 2, height)
-			ctx.stroke()
+      // y center line
+      ctx.strokeStyle = 'red'
+      ctx.beginPath()
+      ctx.moveTo(width / 2, 0)
+      ctx.lineTo(width / 2, height)
+      ctx.stroke()
 
-			// x center line
-			ctx.beginPath()
-			ctx.moveTo(0, height / 2)
-			ctx.lineTo(width, height / 2)
-			ctx.stroke()
+      // x center line
+      ctx.beginPath()
+      ctx.moveTo(0, height / 2)
+      ctx.lineTo(width, height / 2)
+      ctx.stroke()
 
-			ctx.strokeStyle = 'hotpink'
-			ctx.beginPath()
+      ctx.strokeStyle = 'hotpink'
+      ctx.beginPath()
 
-			let cx = 0
-			let cy = height / 2
-			ctx.moveTo(cx, cy) // set start
-			for (let i = 0; i < 200; i++) {
-				let x = i * 3
-				let y = Math.sin(10 * i + t / 180 * Math.PI) * 200
-				ctx.lineTo(cx + x, cy + y)
-			}
-			ctx.stroke()
+      let cx = 0
+      let cy = height / 2
+      ctx.moveTo(cx, cy) // set start
+      for (let i = 0; i < 200; i++) {
+        let x = i * 3
+        let y = Math.sin(10 * i + (t / 180) * Math.PI) * 200
+        ctx.lineTo(cx + x, cy + y)
+      }
+      ctx.stroke()
 
+      // for (let i = 0; i < 5; i++) {
+      // 	for (let j = 0; j < 5; j++) {
+      // 		ctx.beginPath();
+      // 		const x = 25 + j * 50; // x coordinate
+      // 		const y = 25 + i * 50; // y coordinate
+      // 		const radius = 20; // Arc radius
+      // 		const startAngle = 0; // Starting point on circle
+      // 		const endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+      // 		const counterclockwise = i % 2 !== 0; // clockwise or counterclockwise
+      //
+      // 		ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
+      //
+      // 		if (i > 1) {
+      // 			ctx.fill();
+      // 		} else {
+      // 			ctx.stroke();
+      // 		}
+      // 	}
+      // }
 
-			// for (let i = 0; i < 5; i++) {
-			// 	for (let j = 0; j < 5; j++) {
-			// 		ctx.beginPath();
-			// 		const x = 25 + j * 50; // x coordinate
-			// 		const y = 25 + i * 50; // y coordinate
-			// 		const radius = 20; // Arc radius
-			// 		const startAngle = 0; // Starting point on circle
-			// 		const endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
-			// 		const counterclockwise = i % 2 !== 0; // clockwise or counterclockwise
-			//
-			// 		ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
-			//
-			// 		if (i > 1) {
-			// 			ctx.fill();
-			// 		} else {
-			// 			ctx.stroke();
-			// 		}
-			// 	}
-			// }
-
-			/*ctx.clearRect(0, 0, width, height)
+      /*ctx.clearRect(0, 0, width, height)
 			ctx.save()
 			ctx.translate(width / 2, height / 2)
 			ctx.rotate(time * 0.0001)
@@ -87,24 +86,24 @@
 
 			ctx.restore()*/
 
-			// FPS Counter
-			fps = (1 / ((time - pTime) / 1000)).toFixed(1)
-			pTime = time
-		})
-	})
+      // FPS Counter
+      fps = (1 / ((time - pTime) / 1000)).toFixed(1)
+      pTime = time
+    })
+  })
 
-	onMount(() => {
-		const observer = new ResizeObserver(() => {
-			canvas.width = canvas.clientWidth
-			canvas.height = canvas.clientHeight - 1
-		})
+  onMount(() => {
+    const observer = new ResizeObserver(() => {
+      canvas.width = canvas.clientWidth
+      canvas.height = canvas.clientHeight - 1
+    })
 
-		observer.observe(canvas)
-		return () => observer.disconnect()
-	})
+    observer.observe(canvas)
+    return () => observer.disconnect()
+  })
 </script>
 
 <main class="flex">
-	<samp class="fixed inset-0 text-right -z-10">FPS {fps}</samp>
-	<canvas class="block h-full w-full bg-black" bind:this={canvas}></canvas>
+  <samp class="fixed inset-0 text-right -z-10">FPS {fps}</samp>
+  <canvas class="block h-full w-full bg-black" bind:this={canvas}></canvas>
 </main>
